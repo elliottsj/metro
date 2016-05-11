f5922188affd54acb0597d2fe87e446964682ba8
Jorge Bucaran <j@bucaran.me>
2016-05-12 05:02:35 +0900

    Powerline prompt optimized for speed and space.

---
+++ functions/set_color_custom.fish
@@ -0,0 +1,24 @@
+function set_color_custom
+    set -U fish_color_normal                normal
+    set -U fish_color_command               999
+    set -U fish_color_param                 white
+    set -U fish_color_redirection           yellow
+    set -U fish_color_comment               222
+    set -U fish_color_error                 red
+    set -U fish_color_escape                yellow
+    set -U fish_color_operator              yellow
+    set -U fish_color_end                   yellow
+    set -U fish_color_quote                 yellow
+    set -U fish_color_autosuggestion        333
+    set -U fish_color_valid_path            --underline
+    set -U fish_color_cwd                   yellow
+    set -U fish_color_cwd_root              yellow
+    set -U fish_color_match                 yellow
+    set -U fish_color_search_match          --background=magenta
+    set -U fish_color_selection             --background=magenta
+    set -U fish_pager_color_prefix          yellow
+    set -U fish_pager_color_completion      white
+    set -U fish_pager_color_description     888
+    set -U fish_pager_color_progress        yellow
+    set -U fish_color_history_current       yellow
+end
