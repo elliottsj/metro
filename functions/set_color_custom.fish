a8228824b893efee42273dcf17336febcd894e39
Spencer Elliott <me@elliottsj.com>
2016-05-11 17:14:23 -0400

    Brighten `fish_color_comment` and `fish_color_autosuggestion` to 444

--- functions/set_color_custom.fish
+++ functions/set_color_custom.fish
@@ -3,13 +3,13 @@ function set_color_custom
     set -U fish_color_command               999
     set -U fish_color_param                 white
     set -U fish_color_redirection           yellow
-    set -U fish_color_comment               222
+    set -U fish_color_comment               444
     set -U fish_color_error                 red
     set -U fish_color_escape                yellow
     set -U fish_color_operator              yellow
     set -U fish_color_end                   yellow
     set -U fish_color_quote                 yellow
-    set -U fish_color_autosuggestion        333
+    set -U fish_color_autosuggestion        444
     set -U fish_color_valid_path            --underline
     set -U fish_color_cwd                   yellow
     set -U fish_color_cwd_root              yellow
